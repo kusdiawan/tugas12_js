17e8e6c7fd36faaeb64aecaee5934067a84dcd39
kusdiawan <71364605+kusdiawan@users.noreply.github.com>
2020-09-21 08:46:38 +0700

    Add files via upload

---
+++ tugas12.js
@@ -0,0 +1,12 @@
+//mendefinisikan variable array nama-nama Buah
+var namaBuah = ["Pisang", "Jeruk", "Apel", "Mangga"];
+//menampilkan data nama-nama buah
+console.log(namaBuah);
+
+//menghapus data awal dengan shift 
+namaBuah.shift();
+console.log(namaBuah);
+
+//menghapus akhir dengan pop pad array 
+namaBuah.pop();
+console.log(namaBuah);
